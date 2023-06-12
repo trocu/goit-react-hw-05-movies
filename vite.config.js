550db8e87a6e03629bd21d9676a7a9b57a1de06e
Mariusz Trocewicz <throcu@gmail.com>
2023-06-12 23:59:09 +0200

    Added base to vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/goit-react-hw-05-movies',
+  base: '/goit-react-hw-05-movies',
 });
